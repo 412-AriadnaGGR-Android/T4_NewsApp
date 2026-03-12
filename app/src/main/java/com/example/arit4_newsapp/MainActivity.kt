f665bcf6c19805a258c7f322b10624b447469784
AriRodo111 <aribb400@gmail.com>
2026-03-11 23:16:43 -0600

    subo imagenes y empizo con el codigo

---
+++ app/src/main/java/com/example/arit4_newsapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.arit4_newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+data class Noticia(val titulo: String, val fecha: String, val imagen: Int)
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Color.White
+            ) {
+                PantallaNoticias()
+            }
+        }
+    }
+}
+
+val MiColorMorado = Color(0xFF4A2EFF)
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewPantallaNoticias() {
+    PantallaNoticias()
+}
